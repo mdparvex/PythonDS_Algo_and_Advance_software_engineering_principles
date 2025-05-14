31c272c881d7e796f5bd2c58e8c0da1150cce4ea
MD. ABDULLA AL MAMUN <abdulla.mamun@netmediamobile.com>
2025-05-14 22:36:08 +0600

    recursion problem solved

---
+++ recursion/subsets.py
@@ -0,0 +1,16 @@
+def subsets(nums):
+    if not nums:
+        return [[]]
+    
+    first_element = nums[0]
+    rest_element = nums[1:]
+
+    subset_without_first = subsets(rest_element)
+
+    subset_with_first = []
+
+    for set in subset_without_first:
+        subset_with_first.append([first_element] +set)
+    return subset_without_first + subset_with_first
+
+print(subsets([1,2,3]))
